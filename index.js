ce901c610d82f602e7ce64cf823fdc23e1ddbbc4
kwak123 <kwak123@gmail.com>
2018-11-01 22:22:42 -0400

    listening to slack

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const redis = require('./src/db/redis');
+/* eslint-disable-line */ const dotenv = require('dotenv').config();
+/* eslint-disable-line */ const bodyParser = require('body-parser');
+/* eslint-disable-line */ const redis = require('./src/db/redis');
 
 const { NODE_ENV_PROD } = require('./constants');
 
@@ -13,6 +15,22 @@ const logger = (req, res, next) => {
   next();
 };
 app.use(logger);
+app.use(bodyParser.json());
+
+/* Routing */
+app.get('/', (req, res) => {
+  res.send('Sup');
+});
+
+// Challenge
+app.post('/', (req, res) => {
+  console.log(req.body);
+  const { challenge, type } = req.body;
+  if (type !== 'url_verification') {
+    return res.send(challenge);
+  }
+  return res.sendStatus(200);
+});
 
 const port = process.env.NODE_ENV === NODE_ENV_PROD ? 8080 : 3001;
 app.listen(port, () => console.log(`now listening on port ${port}`));
