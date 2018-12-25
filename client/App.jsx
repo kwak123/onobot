3b730b0e1dd4973d3c109866c4d5b2f347599f80
kwak123 <kwak123@gmail.com>
2018-12-25 13:53:54 -0500

    convert to scss, render message list

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
+// Data
+import testMessages from '../testMessages';
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.messageKeys = Object.keys(testMessages);
+  }
+
   tempMethod = () => console.log('hello');
 
   render() {
     return (
-      <div onClick={this.tempMethod}>Hello</div>
+      <div>
+        <ul>
+          {this.messageKeys.map((key) => {
+            /* eslint-disable camelcase */
+            const { client_msg_id } = testMessages[key];
+            return (
+              <li key={client_msg_id}>{key}</li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
